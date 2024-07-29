9dd9ccd5787ce80a35a4a7dceb6067933350660d
Davy Bellens <davybellens123@gmail.com>
2024-07-29 16:46:18 +0200

    WIP feedback

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
+import Header from "@/components/navigation/Header";
+
 export default function Home() {
+  const liStyle = "cursor-pointer text-2xl hover:text-teal-500 font-bold m-5";
   return (
     <>
+      <Header title="Home" />
       <div className="flex flex-col items-center justify-center min-h-screen bg-v-light-green">
         <h1 className="text-4xl text-v-green font-bold text-white">
           Welcome to
@@ -10,7 +14,18 @@ export default function Home() {
             Vliruos
           </h1>
         </div>
-        <div className=""></div>
+        <div>
+          <h1 className="text-4xl text-v-green font-bold text-white">
+            Sharing minds, changing lives
+          </h1>
+        </div>
+        <p>Quick links</p>
+        <ul className="bg-v-green text-white p-4 flex flex-row items-center justify-center">
+          <li className={liStyle}>Dashboards</li>
+          <li className={liStyle}>Learn</li>
+          <li className={liStyle}>Inspire & connect</li>
+          <li className={liStyle}>FAQ</li>
+        </ul>
       </div>
     </>
   );
