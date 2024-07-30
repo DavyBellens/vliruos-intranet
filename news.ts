e77ade35192c88c008cf47cd3cb88100b8bd6484
DavyBellens <r0882857@ucll.be>
2024-07-30 23:19:20 +0200

    dummy data for homepage

---
+++ news.ts
@@ -0,0 +1,22 @@
+const news = [
+  {
+    img: "/images/scrum.webp",
+    alt: "AI generated picture of people doing a scrum meeting",
+    title: "The importance of a scrum meeting",
+    link: "https://www.scrum.org/resources/what-is-a-daily-scrum",
+  },
+  {
+    img: "/images/intern.webp",
+    alt: "AI generated picture of a blonde programmer",
+    title: "10 reasons why we should hire our latest intern again",
+    link: "https://davybellens.com/qualities.html",
+  },
+  {
+    img: "/images/dog.webp",
+    alt: "AI generated picture of a dog",
+    title: "Why you need a dog in the office",
+    link: "https://hbr.org/2023/11/research-the-benefits-of-a-pet-friendly-workplace",
+  },
+];
+
+export default news;
